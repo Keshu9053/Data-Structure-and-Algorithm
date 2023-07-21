1c2c8409afb88ae7677ce91e21c5aeca670ebfa6
Keshav Bhardwaj <103336493+Keshu9053@users.noreply.github.com>
2023-07-21 10:47:15 +0530

    Time: 17 ms (67.86%) | Memory: 16.5 MB (63.42%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums[0] == 0) return 0; 
+        int jump = 0;
+        int des = 0;
+        int pos = 0;
+       
+        for(int i = 0;i<nums.size()-1;++i) {
+            des = max(des,nums[i]+i);
+            if(i == pos) {
+                jump++;
+                pos = des;
+            }
+        }
+        return jump;
+    }
+};
